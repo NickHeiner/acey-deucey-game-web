2db6ab236da4a115d8a39ba267c2055e789a7b62
KatherineThompson <kethompson29@gmail.com>
2016-04-15 18:39:30 -0400

    Adjusted space directive to work for bottom spaces

--- app/space.js
+++ app/space.js
@@ -4,17 +4,27 @@ const angular = require("angular");
 
 angular.module("acey-deucey").directive("space", function() {
     return {
-        template: `<svg class="space" viewbox="0 0 100 350">
+        template: `<svg ng-class="orientationParams.svgClass" viewbox="0 0 100 350">
                         <polygon ng-attr-points="{{orientationParams.polygonPoints}}"/>
-                        <g class="white piece" transform="scale(.75), translate(17, -105)">
+                        <g class="white piece" ng-attr-transform="scale(.75), {{orientationParams.groupTransform}}">
                             <circle cx="50" cy="175" r="50"/>
                             <text font-size="40" x="50%" y="50%" dy="0.3em" text-anchor="middle">5x</text>
                         </g>
                     </svg>`,
         link: function(scope) {
-            scope.orientationParams = {
-                polygonPoints: "0,0 100,0 50,350"
-            };
+            if (scope.orientation === "bottom") {
+                scope.orientationParams = {
+                    polygonPoints: "0,350 100,350 50,0",
+                    groupTransform: "translate(17, 220)",
+                    svgClass: "space bottom"                    
+                }
+            } else {
+                scope.orientationParams = {
+                    polygonPoints: "0,0 100,0 50,350",
+                    groupTransform: "translate(17, -105)",
+                    svgClass: "space top"
+                };
+            }
         },
         scope: {
             orientation: "@"
